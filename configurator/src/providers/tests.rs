9703bdbb302b2bba59ce11c7a131ea0405c11c11
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2026-02-16 14:19:00 +0100

    update libcosmic and make test more usefull (#14)

--- configurator/src/providers/tests.rs
+++ configurator/src/providers/tests.rs
@@ -1,6 +1,8 @@
 use std::{collections::HashMap, fs, marker::PhantomData, path::Path, sync::LazyLock};
 
 use configurator_utils::ConfigFormat;
+use cosmic::cosmic_config::CosmicConfigEntry;
+use dir_assert::assert_paths;
 use figment::{
     value::{Tag, Value},
     Profile, Provider,
@@ -14,12 +16,28 @@ use pretty_assertions::assert_eq;
 
 /// 1. write the value
 /// 2. read the value and assert equal
-fn write_and_read<P: AsRef<Path>>(path: P, format: &ConfigFormat, initial_value: &Value) {
-    let _ = fs::remove_dir_all(path.as_ref());
+fn write_and_read<P: AsRef<Path>, S>(path: P, format: &ConfigFormat)
+where
+    S: Default + Serialize + CosmicConfigEntry,
+{
+    let _ = fs::remove_dir_all("tests");
+    let default_config = S::default();
+    let path = path.as_ref();
 
-    super::write(path.as_ref(), format, initial_value).unwrap();
+    {
+        let config_handler =
+            cosmic::cosmic_config::Config::with_custom_path("expected", 1, "tests".into()).unwrap();
+        default_config.write_entry(&config_handler).unwrap();
+    }
 
-    let value = super::read_from_format(path.as_ref(), format);
+
+    let initial_value = &Value::serialize(default_config).unwrap();
+
+    super::write(path, format, initial_value).unwrap();
+
+    assert_paths(path, "tests/cosmic/expected/v1").unwrap();
+
+    let value = super::read_from_format(path, format);
 
     let value = value.data().unwrap().remove(&Profile::Default).unwrap();
 
@@ -28,12 +46,8 @@ fn write_and_read<P: AsRef<Path>>(path: P, format: &ConfigFormat, initial_value:
     assert_eq!(initial_value, &value);
 }
 
-fn write_and_read_common<S: Default + Serialize>(format: &ConfigFormat) {
-    write_and_read(
-        "tests/cosmic_ron/config1",
-        format,
-        &Value::serialize(S::default()).unwrap(),
-    );
+fn write_and_read_common<S: Default + Serialize + CosmicConfigEntry>(format: &ConfigFormat) {
+    write_and_read::<_, S>("tests/cosmic/actual/v1", format);
 }
 
 #[test]
