5ffc1e4343d9661ace27220551d034b2089bdf7a
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2026-02-20 00:48:30 +0100

    ff

--- rust_schema/src/lib.rs
+++ rust_schema/src/lib.rs
@@ -1,17 +1,22 @@
-use std::collections::BTreeMap;
+use std::{
+    collections::{BTreeMap, HashMap},
+    fs,
+};
 
 use facet::Facet;
+use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 enum Value {
     Unit,
+    Null,
     Bool(bool),
     Number(i32),
     Char(char),
     String(String),
     Array(Vec<Value>),
     Tuple(Vec<Value>),
-    Map(BTreeMap<Value, Value>),
+    Map(BTreeMap<String, Value>),
     UnitStruct(String),
     Struct(String, BTreeMap<String, Value>),
     TupleStruct(String, Vec<Value>),
@@ -20,11 +25,21 @@ enum Value {
     EnumVariantStruct(String, BTreeMap<String, Value>),
 }
 
-type RustSchemaId = u64;
+struct RustSchemaRoot {
+    schema: RustSchemaOrRef,
+    definitions: HashMap<RustSchemaId, RustSchema>,
+}
 
 struct RustSchema {
     kind: RustSchemaKind,
     default: Option<Value>,
+}
+
+type RustSchemaId = u64;
+
+enum RustSchemaOrRef {
+    Ref(RustSchemaId),
+    Schema(RustSchema),
 }
 
 enum RustSchemaKind {
@@ -33,18 +48,23 @@ enum RustSchemaKind {
     Number,
     Char,
     String,
-    Array(RustSchemaId),
-    Tuple(Vec<RustSchemaId>),
-    Map(RustSchemaId, RustSchemaId),
-    Struct(String, BTreeMap<String, RustSchemaId>),
-    TupleStruct(String, Vec<RustSchemaId>),
+    Option(RustSchemaOrRef),
+    Array(RustSchemaOrRef),
+    Tuple(Vec<RustSchemaOrRef>),
+    Map(RustSchemaOrRef),
+    Struct(String, BTreeMap<String, RustSchemaOrRef>),
+    TupleStruct(String, Vec<RustSchemaOrRef>),
     Enum(String, Vec<(String, EnumVariantKind)>),
 }
 
 enum EnumVariantKind {
     Unit,
-    Tuple(Vec<RustSchemaId>),
-    Struct(BTreeMap<String, RustSchemaId>),
+    Tuple(Vec<RustSchemaOrRef>),
+    Struct(BTreeMap<String, RustSchemaOrRef>),
+}
+
+fn gen_rust_schema() -> RustSchemaRoot {
+    todo!()
 }
 
 enum A {
@@ -53,22 +73,21 @@ enum A {
     C { a: u32 },
 }
 
-#[derive(Facet, Debug, Serialize, Deserialize)]
+#[derive(Facet, Debug, Serialize, Deserialize, JsonSchema, Default)]
+struct Y {
+    x: u32,
+    c: Option<Box<Config>>,
+}
+
+#[derive(Facet, Debug, Serialize, Deserialize, JsonSchema, Default)]
 #[serde(default)]
 struct Config {
     #[facet(default)]
     x: (u32, String),
     #[facet(default)]
     y: u32,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            x: Default::default(),
-            y: 88,
-        }
-    }
+    c: Option<Box<Config>>,
+    d: Y,
 }
 
 #[test]
@@ -76,4 +95,15 @@ fn it_works() {
     dbg!(Config::SHAPE);
 
     let config = Config::default();
+}
+
+#[test]
+fn schema() {
+    let schema = schemars::schema_for!(Config);
+
+    let value = json::value::to_value(&schema).unwrap();
+
+    let str = json::to_string_pretty(&value).unwrap();
+
+    fs::write("schema2.json", str.as_bytes()).unwrap();
 }
