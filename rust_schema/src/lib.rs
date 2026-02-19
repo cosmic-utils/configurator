b32c7f0960eabbdd8a86ef7cee93acda3cd59363
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2026-02-19 23:41:41 +0100

    ff

--- rust_schema/src/lib.rs
+++ rust_schema/src/lib.rs
@@ -1,20 +1,78 @@
-use facet::Facet;
+use std::collections::BTreeMap;
 
-#[derive(Facet, Debug, Default)]
+use facet::Facet;
+use serde::{Deserialize, Serialize};
+
+enum Value {
+    Unit,
+    Bool(bool),
+    Number(i32),
+    Char(char),
+    String(String),
+    Array(Vec<Value>),
+    Tuple(Vec<Value>),
+    Map(BTreeMap<Value, Value>),
+    UnitStruct(String),
+    Struct(String, BTreeMap<String, Value>),
+    TupleStruct(String, Vec<Value>),
+    EnumVariantUnit(String),
+    EnumVariantTuple(String, Vec<Value>),
+    EnumVariantStruct(String, BTreeMap<String, Value>),
+}
+
+type RustSchemaId = u64;
+
+struct RustSchema {
+    kind: RustSchemaKind,
+}
+
+enum RustSchemaKind {
+    Unit,
+    Bool,
+    Number,
+    Char,
+    String,
+    Array(RustSchemaId),
+    Tuple(Vec<RustSchemaId>),
+    Map(RustSchemaId, RustSchemaId),
+    Struct(String, BTreeMap<String, RustSchemaId>),
+    TupleStruct(String, Vec<RustSchemaId>),
+    Enum(String, Vec<(String, Option<EnumVariantKind>)>),
+}
+
+enum EnumVariantKind {
+    Unit,
+    Tuple(Vec<RustSchemaId>),
+    Struct(BTreeMap<String, RustSchemaId>),
+}
+
+enum A {
+    A,
+    B(u32),
+    C { a: u32 },
+}
+
+#[derive(Facet, Debug, Serialize, Deserialize)]
+#[serde(default)]
 struct Config {
+    #[facet(default)]
     x: (u32, String),
+    #[facet(default)]
     y: u32,
 }
 
-
-
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            x: Default::default(),
+            y: 88,
+        }
+    }
+}
 
 #[test]
 fn it_works() {
-
     dbg!(Config::SHAPE);
 
-
     let config = Config::default();
-
 }
